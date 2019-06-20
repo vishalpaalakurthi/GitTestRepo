6d5b02f2586b7852eb4f768189a5cbf16d5835d5
Vishal Paalakurthi <vishalpaalakurthi@gmail.com>
2019-06-20 07:26:10 +0530

    first commit

---
+++ src/main/java/com/GitTestRepo/GitTestRepoApplication.java
@@ -0,0 +1,13 @@
+package com.GitTestRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GitTestRepo/GitTestRepoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.GitTestRepo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitTestRepoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
